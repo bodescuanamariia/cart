4cfcf9b66becacaa5a7809ff1e971630c72b9c34
Bodescu Anamaria <bodescu.anamariia@gmail.com>
2022-05-05 09:41:41 +0300

    first commit

---
+++ .history/src/App_20220505093907.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ .history/src/App_20220505093916.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+
+// components
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
+// items
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
